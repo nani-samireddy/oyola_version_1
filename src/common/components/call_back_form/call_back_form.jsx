23359d509df8c2d7a506e6d876f23b9b4bf72e62
nani-samireddy <nanisamireddy05@gmail.com>
2023-09-20 09:11:50 +0530

    updated the footer

---
+++ src/common/components/call_back_form/call_back_form.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function CallBackForm() {
+  return (
+   <div className="w-full flex flex-col justify-center items-center mt-20">
+        <h2 className="text-5xl font-bold font-dm_sans text-gray-700 my-5 text-center">
+                            Book your stay
+                        </h2>
+      <h2 className="text-xl lg:text-2xl font-light font-dm_sans text-gray-400  text-center md:px-6 lg:px-24">
+                            Our executive will contact you within 24 hours to confirm your booking and payment details.
+                            Or you can call us at <a href="tel:+919618434488" className="text-gray-900 font-semibold">+91 9618434488</a> or <a href="tel:098936293565" className="text-gray-900 font-semibold">098936293565</a> for instant booking.
+                </h2>
+        <form action="https://formsubmit.co/a4c271330781fff43fdb513ce98718" method="POST"  className="flex flex-col my-10 w-full items-center justify-center ">
+        <input type="hidden" name="_template" value="table"/>
+        <input type="hidden" name="_subject" value="New Booking for stays!"/>
+        <input type="text" name="Name" placeholder="Name" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required/>
+        <input type="email" name="Email" placeholder="Email" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required/>
+        <input  type="number" name="Phone" placeholder="Phone" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required/>
+        <select name="Type" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required>
+          <option value="" selected disabled hidden>select the serivce</option>
+          <option value="Stays">Stays</option>
+          <option value="Cars">Cars</option>
+          <option value="Trips">Trips</option>
+          <option value="Trekking">Trekking</option>
+          <option value="Camping">Camping</option>
+          <option value="Others">Others</option>
+        </select>
+        <textarea name="Message" placeholder="Message" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required></textarea>
+        <button type="submit" className="w-full md:w-96 bg-gray-900 text-white rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400">Place a call back</button>
+
+        </form>
+        </div>
+  )
+}
--- src/pages/homepage/stays/stays.jsx
+++ src/pages/homepage/stays/stays.jsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { StaysTypesContext } from "../../../contexts/contexts";
 import { HomeModernIcon } from "@heroicons/react/24/outline";
 import HowItWorks from "../../../common/components/how_it_works/how_it_works";
+import CallBackForm from "../../../common/components/call_back_form/call_back_form";
 
 export default function Stays() {
   const [staysTypes, hasData] = useContext(StaysTypesContext);
@@ -48,34 +49,9 @@ export default function Stays() {
       <HowItWorks />
 
       {/* Booking form */}
-      <div className="w-full flex flex-col justify-center items-center mt-20">
-        <h2 className="text-5xl font-bold font-dm_sans text-gray-700 my-5 text-center">
-                            Book your stay
-                        </h2>
-      <h2 className="text-2xl font-light font-dm_sans text-gray-400  text-center md:px-6 lg:px-24">
-                            Our executive will contact you within 24 hours to confirm your booking and payment details.
-                            Or you can call us at <a href="tel:+919618434488" className="text-gray-900 font-semibold">+91 9618434488</a> or <a href="tel:098936293565" className="text-gray-900 font-semibold">098936293565</a> for instant booking.
-                </h2>
-        <form action="https://formsubmit.co/a4c271330781fff43fdb513ce98718" method="POST"  className="flex flex-col my-10 w-full items-center justify-center ">
-        <input type="hidden" name="_template" value="table"/>
-        <input type="hidden" name="_subject" value="New Booking for stays!"/>
-        <input type="text" name="Name" placeholder="Name" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required/>
-        <input type="email" name="Email" placeholder="Email" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required/>
-        <input  type="number" name="Phone" placeholder="Phone" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required/>
-        <select name="Type" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required>
-          <option value="" selected disabled hidden>select the serivce</option>
-          <option value="Stays">Stays</option>
-          <option value="Cars">Cars</option>
-          <option value="Trips">Trips</option>
-          <option value="Trekking">Trekking</option>
-          <option value="Camping">Camping</option>
-          <option value="Others">Others</option>
-        </select>
-        <textarea name="Message" placeholder="Message" className="w-full md:w-96 border-2 border-gray-200 rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400" required></textarea>
-        <button type="submit" className="w-full md:w-96 bg-gray-900 text-white rounded-lg p-2 my-2 focus:outline-none focus:border-gray-400">Place a call back</button>
-
-        </form>
-        </div>
+      <div>
+        <CallBackForm/>
+      </div>
     </div>
   );
 }
